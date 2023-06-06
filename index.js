7dbbb11640c78c2118465d07a80cea64743effe9
Toktobekov Kylymbek <kylymbekkemin@gmail.com>
2023-06-06 15:05:14 +0600

    wwww

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const jsonServer = require('json-server')
-const auth = require('json-server-auth')
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const cors = require("cors");
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+const domainList = ["http://localhost:3030"];
 
-const app = jsonServer.create()
-const router = jsonServer.router('db.json')
-
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (domainList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 // /!\ Bind the router db to the app
-app.db = router.db
+app.db = router.db;
 
 // You must apply the auth middleware before the router
-app.use(auth)
-app.use(router)
-app.listen(3000)
+app.use(auth);
+app.use(cors(corsOptions));
+app.use(router);
+app.listen(3000);
