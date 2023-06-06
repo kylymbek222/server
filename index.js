0fa0e3ae8695341dbcd8e164e761c531701d0578
Toktobekov Kylymbek <kylymbekkemin@gmail.com>
2023-06-06 16:12:26 +0600

    fgcgc

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const auth = require("json-server-auth");
 const cors = require("cors");
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults({ noCors: true });
 
 // /!\ Bind the router db to the app
 app.db = router.db;
 
 // You must apply the auth middleware before the router
 app.use(auth);
-app.use(middlewares);
 app.use(router);
 app.use(
   cors({
