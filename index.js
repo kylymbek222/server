dbc84ff197d45474543c421a9ed795470e071347
Toktobekov Kylymbek <kylymbekkemin@gmail.com>
2023-06-06 15:33:59 +0600

    jk

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
+const cors = require("cors");
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults({ noCors: true });
-const domainList = ["http://localhost:3030"];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (domainList.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
 // /!\ Bind the router db to the app
 app.db = router.db;
 
@@ -21,4 +12,15 @@ app.db = router.db;
 app.use(auth);
 app.use(middlewares);
 app.use(router);
-app.listen(3000);
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+    preflightContinue: false,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
+app.options("*", cors());
+app.listen(3001, () => {
+  console.log("JSON Server is running");
+});
