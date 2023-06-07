61d54f414fc735b7807fde1cfdbacfd3e5f4897d
Toktobekov Kylymbek <kylymbekkemin@gmail.com>
2023-06-07 16:50:01 +0600

    hjv

--- server.mjs
+++ server.mjs
@@ -1,20 +1,17 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
-const cors = require("cors");
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
 // /!\ Bind the router db to the app
 app.db = router.db;
 
 // You must apply the auth middleware before the router
+
 app.use(auth);
+app.use(middlewares);
 app.use(router);
-app.use(
-  cors({
-    origin: ["http://localhost:3030"]
-  })
-);
-app.listen(3001, () => {
+app.listen(process.env.PORT, () => {
   console.log("JSON Server is running");
 });
